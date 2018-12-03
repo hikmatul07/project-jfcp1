275e30808dbb51ff1bb5aae6a312febe81d3f4d6
maulida <nur.maulida@students.amikom.ac.id>
2018-12-03 16:59:30 +0700

    hapus hallo moli

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
 public class Hello {
 	public static void main (String[] args) {
 	
-		System.out.println("Hello Maulida bali!");
+		
 		
 		System.out.println("Sudah Makan Belum?");
+		
 		
 	}
 	
